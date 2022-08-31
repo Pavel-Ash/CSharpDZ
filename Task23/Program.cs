66d8401eba5805e4bb8be58050c5442a3ca0cf5a
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-08-31 21:43:53 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=x; i++)
+{
+    Console.Write(Convert.ToString(Math.Pow(i, 3)));
+    if (i != x)
+    {
+        Console.Write(", ");
+    }
+}
+
