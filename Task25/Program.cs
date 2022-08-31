3aeffed927648fa8f7129491a13e58bea94371a8
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-08-31 22:45:45 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string Power (int x, int y)
+{
+    return Convert.ToString(Math.Pow(x, y));
+}
+
+Console.WriteLine("Получилось "+Power(x, y));
