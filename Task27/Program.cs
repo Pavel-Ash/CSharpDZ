f3cdc9f876472bfa9bf2e467519582218a39c227
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-08-31 23:34:36 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string Counter (int x)
+{
+    int sum = 0;
+    while (x>0)
+    {
+        sum = sum + x%10;
+        x = (x - x%10) / 10;
+    }
+    return Convert.ToString(sum);
+}
+
+Console.WriteLine("Получилось "+Counter(x));
