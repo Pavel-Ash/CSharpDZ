457ed6ac0f2767b57e0c831ce5db4c40c58dba5b
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-08-31 21:10:21 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string x = Console.ReadLine();
+int y = Convert.ToInt32(x);
+if (x.Length != 5)
+{
+    Console.WriteLine("Мы так не договаривались. Число не пятизначное");
+}
+else
+{
+    if (x[0] == x[4] && x[1] == x[3])
+    {
+        Console.WriteLine("Опа. Да это ж полиндром");
+    }
+    else
+    {
+        Console.WriteLine("На полиндром не похоже");
+    }
+}
+
