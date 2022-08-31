d1e9d3eafdc3c6949f6cac5f9341685843627759
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-09-01 00:08:01 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,27 @@
+﻿int [] Arr (int size)
+{
+    int [] result = new int [size];
+    for (int i=0; i<size; i++)
+    {
+        result[i] = new Random().Next(0, 50);
+    }
+    return result;
+}
+
+void PtintArray(int[] array)
+{
+int count = array.Length;
+Console.Write("[");
+for (int i = 0; i < count; i++)
+{
+    Console.Write(array[i]);
+    if (i < count-1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.Write("]");
+}
+
+PtintArray(Arr(8));
+
