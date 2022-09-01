a3573f07f0a145562298e24380f844112fc1a9aa
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-09-02 01:50:27 +0300

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите размер массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array= new int [n];
+int oddSum = 0;
+
+
+for (int i=0; i<n; i++)
+{
+    array[i] = new Random().Next(1, 9);
+    if (i%2==0)
+    {
+        oddSum = oddSum+array[i];
+    }
+}
+
+Console.WriteLine(String.Join(";", array));
+Console.WriteLine($"Сумма нечетных элементов: {oddSum}");
