f8af9d37b213ac48c72644b558e346d0911001f5
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-09-02 01:37:44 +0300

    Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите размер массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array= new int [n];
+int Counter = 0;
+
+
+for (int i=0; i<n; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    if (array[i]%2==0)
+    {
+        Counter++;
+    }
+}
+
+Console.WriteLine(String.Join(";", array));
+Console.WriteLine($"Четных чисел в массиве: {Counter} ");
