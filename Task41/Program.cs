ace10f1e6eb30bb681d7a79e0c3dabf6e1d6d0b4
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-09-10 19:44:31 +0300

    Homework for lesson6

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите ваши числа через запятую и пробел");
+string? stroka = Console.ReadLine();
+if (stroka == null || stroka.Length < 1)
+{
+    Console.WriteLine("Вы ничего не ввели((");
+}
+else
+{
+    char[] separators = new char[] { ' ', ',' };
+    string[] subs = stroka.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+    int posCounter = 0;
+    foreach (var sub in subs)
+    {
+        if (Convert.ToInt32(sub) > 0)
+        {
+            posCounter++;
+        }
+    }
+    Console.WriteLine($"Количество положительных чисел в строке: {posCounter}");
+}
+
+
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b1-b2)/(k2-k1);
+double y = k1*x + b1;
+Console.WriteLine($"Пересечение в точке ({x}, {y})");
