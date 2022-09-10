bfef314686a48f53d84dbd995e25551c1ff9903f
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-09-10 22:00:31 +0300

    Task 43 fixed

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -7,6 +7,18 @@ double k2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите b2");
 double b2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (b1-b2)/(k2-k1);
-double y = k1*x + b1;
-Console.WriteLine($"Пересечение в точке ({x}, {y})");
+if (k1==k2 && b1 == b2)
+{
+    Console.WriteLine("Пересечение во всех точках. Линии идентичны");
+}
+else
+    if (k1==k2)
+    {
+        Console.WriteLine("Точек пересечения нет. Линии параллельны");
+    }
+    else
+        {
+        double x = (b1-b2)/(k2-k1);
+        double y = k1*x + b1;
+        Console.WriteLine($"Пересечение в точке ({x}, {y})");
+        }
