444e276c66cbe577860202e6384fed0cc01a017a
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-09-19 19:19:18 +0300

    DZ Lesson 8+

---
+++ Task54/Program.cs
@@ -0,0 +1,31 @@
+﻿int m = new Random().Next(2, 5);
+int n = new Random().Next(2, 5);
+
+int [,] matrix = new int [m, n];
+
+for (int i=0; i<m; i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        matrix[i,j] = new Random().Next(-10, 10);
+        Console.Write(matrix[i,j]+"\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i=0; i<m; i++)
+{
+    int [] vector = new int [n];
+    for (int j=0; j<n; j++)
+    {
+        vector[j] = matrix[i,j];
+    }
+    Array.Sort(vector);
+    Array.Reverse(vector);
+    for (int j=0; j<n; j++)
+    {
+        Console.Write(vector[j]+"\t");
+        matrix[i,j] = vector[j];
+    }
+    Console.WriteLine(); 
+}
