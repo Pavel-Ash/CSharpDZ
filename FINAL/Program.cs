0516b1b3c3be81977c58d514b3770ebde0fc3897
Pavel-Ash <62200485+Pavel-Ash@users.noreply.github.com>
2022-10-05 00:41:57 +0300

    FINAL code

---
+++ FINAL/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите элементы массива, используя / в качестве разделителя элементов");
+string? stroka = Console.ReadLine();
+if (stroka == null || stroka.Length < 1)
+{
+    Console.WriteLine("Вы ничего не ввели((");
+}
+else
+{
+    string[] subs = stroka.Split('/');
+    var res = from s in subs where s.Length <=3 select s;
+    Console.WriteLine("Итоговый массив:");
+    Console.Write("[ ");
+    foreach (string s in res)
+        {
+            Console.Write('"'+s+'"');
+            Console.Write(" ");
+        }
+    Console.Write("]");
+}
